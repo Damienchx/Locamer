661554c31f434312f90d8d7ff6c864b9d50282a5
Damienchx <ferreiradacostadamien@gmail.com>
2017-05-15 11:43:39 +0200

    designer update

--- PPE2/Locamer/locamer/Models/locamerModel.Designer.cs
+++ PPE2/Locamer/locamer/Models/locamerModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\leclercq.sylvain\Desktop\PPE2\locamer\locamer\Models\locamerModel.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\leclercq.sylvain\Desktop\PPE2\Locamer\locamer\Models\locamerModel.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
